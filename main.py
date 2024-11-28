05e8a3e4924ffe8d7d62a70533714a7ecce2008d
Nilo Jr. - LowmaxPC <nilojr.olang@gmail.com>
2024-11-28 21:50:41 +0800

    Update .gitignore

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ import tkinter as tk
 from ui_functions import setup_ui
 import random as rd
 
+import sys
+import os
+
+
+def resource_path(relative_path):
+    # Get the absolute path to resources
+    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+icon_path = resource_path("assets/TreasureHunt.ico")
+
 
 class TreasureHuntGame:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Treasure Hunt Duel")
         self.root.geometry("400x800")
-        self.root.iconbitmap("TreasureHunt.ico")
+        self.root.iconbitmap(icon_path)
         self.grid_size = 3
         self.grid_color = "lightblue"  # Initialize grid_color
         self.treasure_location = (
